c725b22d9d09b80f01ed519f696a3ec1c0cadcff
Jay Crisostomo <jaycris2000@gmail.com>
2022-07-09 11:12:01 -0500

    first init

---
+++ src/components/home/Home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './home.css'
+
+function home () {
+  return <div className="home">home</div>
+}
+
+export default home
---
+++ src/components/service/Service.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './service.css'
+
+function Service () {
+  return <div>Service</div>
+}
+
+export default Service
